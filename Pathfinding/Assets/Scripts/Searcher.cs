dcd254c656b9a1f3a433ce2a665f5456717948f4
Cat <cat.calupas@gmail.com>
2020-12-04 13:37:32 +0100

    add searcher script

---
+++ Pathfinding/Assets/Scripts/Searcher.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Searcher : MonoBehaviour
+{
+    public Transform goal;
+    public Graph grid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void BreadthFirstSearch()
+    {
+        Vector2 currentPos = this.transform.position;
+
+        var frontier = new Queue<Vector2>();
+        frontier.Enqueue(currentPos);
+
+        var reached = new HashSet<Vector2>();
+        reached.Add(currentPos);
+
+        while (frontier.Count > 0)
+        {
+            var current = frontier.Dequeue();
+
+            foreach (Vector2 next in grid.Neighbors(current))
+            {
+                if (!reached.Contains(next))
+                {
+                    frontier.Enqueue(next);
+                    reached.Add(next);
+                }
+            }
+        }
+    }
+}
