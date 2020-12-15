5c0b36f7319bfc052878870409b07d03fbf8b040
Cat <cat.calupas@gmail.com>
2020-12-15 15:48:13 +0100

    add PriorityQueue

--- Pathfinding/Assets/Scripts/Searcher.cs
+++ Pathfinding/Assets/Scripts/Searcher.cs
@@ -23,20 +23,24 @@ public class Searcher : MonoBehaviour
 
     Queue<Vector2> BreadthFirstSearch()
     {
+        //point startPos to its nearest node in grid
         Vector2 startPos = this.transform.position;
+        startPos = GetNearestNode(startPos);
 
         var frontier = new Queue<Vector2>();
         frontier.Enqueue(startPos);
 
         var cameFrom = new Dictionary<Vector2, Vector2>();
-        //cameFrom[startPos] = null;
+
+        //point goal position to its nearest node in grid
+        Vector2 pathGoal = GetNearestNode(goal.position);
 
         //breadcrumbs and pointer
         while (frontier.Count > 0)
         {
             var current = frontier.Dequeue();
 
-            if (current == (Vector2)goal.position) //early exit
+            if (current == pathGoal) //early exit
             {
                 break;
             }
@@ -54,7 +58,7 @@ public class Searcher : MonoBehaviour
         }
 
         //path creation
-        var pos = (Vector2)goal.position;
+        var pos = pathGoal;
         Queue<Vector2> path = new Queue<Vector2>();
 
         while(pos != startPos)
@@ -69,7 +73,77 @@ public class Searcher : MonoBehaviour
         return path;
     }
 
+    Queue<Vector2> AStarSearch()
+    {
+        //point startPos to its nearest node in grid
+        Vector2 startPos = this.transform.position;
+        startPos = GetNearestNode(startPos);
 
+        var frontier = new Queue<Vector2>();
+        frontier.Enqueue(startPos);
+
+        var cameFrom = new Dictionary<Vector2, Vector2>();
+
+        //point goal position to its nearest node in grid
+        Vector2 pathGoal = GetNearestNode(goal.position);
+
+        //breadcrumbs and pointer
+        while (frontier.Count > 0)
+        {
+            var current = frontier.Dequeue();
+
+            if (current == pathGoal) //early exit
+            {
+                break;
+            }
+
+            Vector2[] neighbours = grid.graph.Neighbors(current);
+
+            foreach (Vector2 next in neighbours)
+            {
+                if (!cameFrom.ContainsKey(next))
+                {
+                    frontier.Enqueue(next);
+                    cameFrom[next] = current;
+                }
+            }
+        }
+
+        //path creation
+        var pos = pathGoal;
+        Queue<Vector2> path = new Queue<Vector2>();
+
+        while (pos != startPos)
+        {
+            path.Enqueue(pos);
+
+            pos = cameFrom[pos];
+        }
+
+        path = new Queue<Vector2>(path.Reverse());
+
+        return path;
+    }
+
+    Vector2 GetNearestNode(Vector2 currentPos)
+    {
+        var nodesInGrid = grid.graph.edges.Keys.ToArray<Vector2>();
+
+        Vector2 nearestNode = nodesInGrid[0];
+        float distance = Vector2.Distance(nodesInGrid[0], currentPos);
+
+        for (int i = 0; i < nodesInGrid.Length; ++i)
+        {
+            //check distance
+            if (Vector2.Distance(nodesInGrid[i], currentPos) < distance)
+            {
+                distance = Vector2.Distance(nodesInGrid[i], currentPos);
+                nearestNode = nodesInGrid[i];
+            }
+        }
+
+        return nearestNode;
+    }
 
     IEnumerator Walk(Queue<Vector2> path)
     {
@@ -85,22 +159,9 @@ public class Searcher : MonoBehaviour
             }
         }
     }
-}
 
-public class WalkCommand
-{
-    Vector2 goal;
-    public WalkCommand(Vector2 pos)
+    float Heuristic(Vector2 a, Vector2 b)
     {
-        goal = pos;
-    }
-
-    bool Execute(Transform motor)
-    {
-        while ((Vector2)motor.position != goal)
-        {
-            motor.Translate(goal);
-        }
-        return true;
+        return Mathf.Abs(a.x - b.x) + Mathf.Abs(a.y - b.y);
     }
 }
